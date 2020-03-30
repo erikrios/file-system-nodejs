ebf09b6e6d479f0321ba4d246545586c1e9bb075
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-03-30 20:58:00 +0700

    Add code to create a new file

--- fs_append.js
+++ fs_append.js
@@ -3,5 +3,5 @@ var fs = require('fs');
 // Create a file named mynewfile1.txt
 fs.appendFile('mynewfile1.txt', 'Hello from Erik', function (err) {
     if (err) throw err;
-    console.log('Saved');
+    console.log('Saved!');
 });
